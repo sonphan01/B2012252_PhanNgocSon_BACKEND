c60ce65dc933fa84ff84d8a76188aa3232a777cf
Phan Ngoc Son <sonb2012252@student.ctu.edu.vn>
2024-09-23 22:55:49 +0700

    Restart

--- app.js
+++ app.js
@@ -21,9 +21,10 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal server error",
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal server error",
     });
 });
+
 
 module.exports = app;
--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const contacts = require("../controllers/contact.controller");
-const { route } = require("../../app");
 
 const router = express.Router();
 
